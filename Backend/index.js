9f71ded7e42c42026ecb5d990115775e23813735
Amrender kuamr <amrenderk66@gmail.com>
2026-02-11 14:27:22 +0530

    change the routes

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
-
+import userRoutes from './routes/user.route.js';
 
 const app  = express();
+app.use(express.json());
+
+// Import routes
+app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
