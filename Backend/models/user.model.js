6b83b9bef994247e70f55330f48342d07c7bf1d7
Amrender kuamr <amrenderk66@gmail.com>
2026-02-13 22:40:03 +0530

    database write with models

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,2 +1,22 @@
+import mongoose from 'mongoose';
 
+const userShema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    timestamps: true
+})
 
+const User = mongoose.model('User', userShema);
+
+export default User;
