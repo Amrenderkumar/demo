cfa06a992b651940476c295022139e5037f0c269
Amrender kuamr <amrenderk66@gmail.com>
2026-02-13 23:32:25 +0530

    commen my routes method

--- Backend/index.js
+++ Backend/index.js
@@ -13,27 +13,27 @@ const call = [];
 app.use(express.json());
 
 
-app.post('/call', (req,res) => {
-    call.push(req.body);
-    res.status(201).json({ message: 'Call data received successfully!' });
-})
+// app.post('/call', (req,res) => {
+//     call.push(req.body);
+//     res.status(201).json({ message: 'Call data received successfully!' });
+// })
 
-app.get('/call', (req,res) => {
-    res.status(200).json({ message: 'Call data  created', call: call});
-})
+// app.get('/call', (req,res) => {
+//     res.status(200).json({ message: 'Call data  created', call: call});
+// })
 
-app.delete('/call/:index', (req,res)=> {
-    const index = req.params.index;
-    delete call[ index ];
-    res.status(200).json({ message: 'Call data deleted successfully!' });
-})
+// app.delete('/call/:index', (req,res)=> {
+//     const index = req.params.index;
+//     delete call[ index ];
+//     res.status(200).json({ message: 'Call data deleted successfully!' });
+// })
 
-app.patch('/call/:index', (req,res) => {
-    const index = req.params.index;
-    const phone = req.body.phone;
-    call[ index ].phone = phone;
-    res.status(200).json({ message: 'Call data updated successfully!' });
-})
+// app.patch('/call/:index', (req,res) => {
+//     const index = req.params.index;
+//     const phone = req.body.phone;
+//     call[ index ].phone = phone;
+//     res.status(200).json({ message: 'Call data updated successfully!' });
+// })
 
 // const rani = [];
 
