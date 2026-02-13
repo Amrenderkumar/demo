40cf020151e60d3e4d1c15c5302fb26ed50a04d3
Amrender kuamr <amrenderk66@gmail.com>
2026-02-13 22:54:01 +0530

    dotenv add

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import connectDB from './database/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 
 const app  = express();
+connectDB();
 
 const call = [];
 
