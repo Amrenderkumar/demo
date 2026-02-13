1f02915b50b5c53acd1eb97245bd9f607ec50713
Amrender kuamr <amrenderk66@gmail.com>
2026-02-13 23:43:05 +0530

    change the server and the create a new file

--- Backend/index.js
+++ Backend/index.js
@@ -1,67 +1,18 @@
-import express from 'express';
+// require a express server
+import app from './src/app.js';
 import connectDB from './database/db.js';
 import dotenv from 'dotenv';
 
+
+
 dotenv.config();
-
-
-const app  = express();
 connectDB();
 
-const call = [];
-
-app.use(express.json());
-
-
-// app.post('/call', (req,res) => {
-//     call.push(req.body);
-//     res.status(201).json({ message: 'Call data received successfully!' });
-// })
-
-// app.get('/call', (req,res) => {
-//     res.status(200).json({ message: 'Call data  created', call: call});
-// })
-
-// app.delete('/call/:index', (req,res)=> {
-//     const index = req.params.index;
-//     delete call[ index ];
-//     res.status(200).json({ message: 'Call data deleted successfully!' });
-// })
-
-// app.patch('/call/:index', (req,res) => {
-//     const index = req.params.index;
-//     const phone = req.body.phone;
-//     call[ index ].phone = phone;
-//     res.status(200).json({ message: 'Call data updated successfully!' });
-// })
-
-// const rani = [];
-
-// app.post('/rani', (req,res) => {
-
-//     rani.push(req.body);
-//     res.status(201).json({ message: 'Rani data received successfully!' });
-// })
-
-// app.get ('/rani', (req,res)=> {
-//     res.status(200).json({ message: 'Rani data created', rani: rani});
-// })
-
-// app.delete('/rani/:index',(req,res)=> {
-//     const index = req.params.index;
-//     delete rani[index ];
-//     res.status(200).json({ message: 'Rani data deleted successfully!' });
-// })
-
-// app.patch('/rani/:index', (req,res)=> {
-//     const index = req.params.index;
-//     const title = req.body.title;
-//     rani[index].title = title;
-//     res.status(200).json({ message: 'Rani data updated successfully!' });
-// })
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
 
---
+++ Backend/src/app.js
@@ -0,0 +1,55 @@
+import express from 'express';
+
+const app  = express();
+
+// const call = [];
+
+
+// app.post('/call', (req,res) => {
+//     call.push(req.body);
+//     res.status(201).json({ message: 'Call data received successfully!' });
+// })
+
+// app.get('/call', (req,res) => {
+//     res.status(200).json({ message: 'Call data  created', call: call});
+// })
+
+// app.delete('/call/:index', (req,res)=> {
+//     const index = req.params.index;
+//     delete call[ index ];
+//     res.status(200).json({ message: 'Call data deleted successfully!' });
+// })
+
+// app.patch('/call/:index', (req,res) => {
+//     const index = req.params.index;
+//     const phone = req.body.phone;
+//     call[ index ].phone = phone;
+//     res.status(200).json({ message: 'Call data updated successfully!' });
+// })
+
+// const rani = [];
+
+// app.post('/rani', (req,res) => {
+
+//     rani.push(req.body);
+//     res.status(201).json({ message: 'Rani data received successfully!' });
+// })
+
+// app.get ('/rani', (req,res)=> {
+//     res.status(200).json({ message: 'Rani data created', rani: rani});
+// })
+
+// app.delete('/rani/:index',(req,res)=> {
+//     const index = req.params.index;
+//     delete rani[index ];
+//     res.status(200).json({ message: 'Rani data deleted successfully!' });
+// })
+
+// app.patch('/rani/:index', (req,res)=> {
+//     const index = req.params.index;
+//     const title = req.body.title;
+//     rani[index].title = title;
+//     res.status(200).json({ message: 'Rani data updated successfully!' });
+// })
+
+export default app;
