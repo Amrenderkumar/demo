f96914c6b48f7169f15db04f9462e6ca5c85cd69
Amrender kuamr <amrenderk66@gmail.com>
2026-02-21 13:45:05 +0530

    add header makes

--- Frontend/src/component/Header.jsx
+++ Frontend/src/component/Header.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      
+      <div>
+        <h1>Demo</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, atque?</p>
+      </div>
     </div>
   )
 }
